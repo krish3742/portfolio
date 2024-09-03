4aa71267ac5753327b403d6948658968f1029ca7
krish3742 <krish3742@gmail.com>
2024-09-03 19:22:18 +0530

    Designed Projects section and made some of the projects functional

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,40 @@
+import Style from './Projects.module.css';
+
 function Projects(props) {
+    const myProject = [
+        {id: 1, name: 'Quizzard - A Quiz Manager', description: 'Developed a Quiz Manager app for seamless quiz creation, editing, publishing, and analysis capabilities.', stack: 'MERN Stack, Tailwind', code: 'https://github.com/krish3742/HostedProjects/tree/main/Quizzard', live: 'https://quizzard-acwk.onrender.com/'},
+        {id: 2, name: 'Tic-Tac-Toe', description: 'Developed a Tic-Tac-Toe game, allowing users to play and track their moves in real-time.', stack: 'React', code: 'https://github.com/krish3742/TriwebAPI-Learning/tree/main/Projects/Tic_Tac_Toe/tic_tac_toe', live: 'b2'},
+        {id: 3, name: 'Drawing App', description: 'Developed a drawing playground, allowing users to select different colors, draw, erase and reset.', stack: 'Vanilla JS', code: 'https://github.com/krish3742/Mini-Projects/tree/main/Drawing_App', live: 'https://drawing-app-chi-brown.vercel.app/'},
+        {id: 4, name: 'Portfolio Website', description: 'Developed a portfolio website, allowing users to view my introduction, skills, download resume, and contact me.', stack: 'React, Tailwind', code: 'https://github.com/krish3742/portfolio', live: 'https://kshitijcodes.onrender.com/'},
+        {id: 5, name: 'TO-DO App', description: 'Developed a To-Do app, allowing users to set task, hold task, remove task, add task to favorites.', stack: 'MERN Stack', code: 'https://github.com/krish3742/TriwebAPI-Learning/tree/main/Projects/MERN%20Todo%20App', live: 'https://drawing-app-chi-brown.vercel.app/'}
+    ];
     return (
         <>
-            <div ref={props.project}>
-                I'm project
+            <div ref={props.project} className={Style.container}>
+                <div className={Style.firstContainers}>
+                    <h1 className={Style.heading}>My Projects</h1>
+                </div>
+                <div className={Style.secondContainers}>
+                    {myProject.map((project) => {
+                        return (
+                            <div className={Style.project} key={project.id}>
+                                <div className={Style.imgDiv}>
+                                    <div className={Style.img}/>
+                                </div>
+                                <h3 className={Style.projectHeading}>{project.name}</h3>
+                                <div className={Style.stackDiv}>
+                                    <p className='font-semibold pr-2'>Tech Stack:</p>
+                                    <p> {project.stack}</p>
+                                </div>
+                                <p className={Style.projectPara}>{project.description}</p>
+                                <div className={Style.buttonDiv}>
+                                    <button className={Style.button} onClick={() => window.open(project.code, '_blank')}>View Code</button>
+                                    <button className={Style.button} onClick={() => window.open(project.live, '_blank')}>View Website</button>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </>
     );
