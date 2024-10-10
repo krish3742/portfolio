0f60ff0e4c70785bd6e2827e709a52a87057dc6b
krish3742 <krish3742@gmail.com>
2024-10-10 14:19:44 +0530

    Updated percentage and cpga in resume and portfolio

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -23,19 +23,19 @@ function Experience(props) {
                                     <p className={Style.years}>2020 - 2024</p>
                                     <h4 className='font-bold'>B.Tech</h4>
                                     <p className='min-w-full'>Galgotias University</p>
-                                    <p>7.19 CGPA</p>
+                                    <p>7.83 CGPA</p>
                                 </div>
                                 <div className={Style.iContent}>
                                     <p className={Style.years}>2019 - 2020</p>
                                     <h4 className='font-bold'>Intermediate</h4>
                                     <p>NIOS Board</p>
-                                    <p>PCM - 72%</p>
+                                    <p>PCM - 72.86%</p>
                                 </div>
                                 <div className={Style.hContent}>
                                     <p className={Style.years}>2016 - 2017</p>
                                     <h4 className='font-bold min-w-full'>High School</h4>
                                     <p>ICSE Board</p>
-                                    <p>PCMB - 75%</p>
+                                    <p>PCMB - 75.33%</p>
                                 </div>
                             </div>
                         </div>
