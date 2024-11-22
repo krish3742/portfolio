b21218b6c19860c7479f5e5302514266700aa790
krish3742 <krish3742@gmail.com>
2024-11-22 14:16:01 +0530

    Added college project

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,22 +12,46 @@ function Education(props) {
             <h4 className="font-bold">
               B.Tech, <span className={Style.para}>Galgotias University</span>
             </h4>
-            <p className={Style.years}>2020 - 2024</p>
             <p>7.83 CGPA</p>
+            <p className={Style.years}>2020 - 2024</p>
+            <p className="font-bold">
+              Project: Credit Card Fraud Detection System
+            </p>
+            <ul className={Style.listpara}>
+              <li>
+                Developed a machine learning model to detect fraudulent credit
+                card transactions, prioritizing high accuracy and low false
+                positive rates.
+              </li>
+              <li>
+                Evaluated and compared multiple machines learning algorithms,
+                including Decision Trees, Support Vector Machine, and Neural
+                Networks.
+              </li>
+              <li>
+                Trained and tested models using a real-world dataset of credit
+                card transactions, consisting of both legitimate and fraudulent
+                cases.
+              </li>
+              <li>
+                Achieved high accuracy, with SVM and Neural Networks
+                demonstrating the best performance.
+              </li>
+            </ul>
           </div>
           <div className={Style.content}>
             <h4 className="font-bold">
               Intermediate, <span className={Style.para}>NIOS Board</span>
             </h4>
-            <p className={Style.years}>2019 - 2020</p>
             <p>PCM - 72.86%</p>
+            <p className={Style.years}>2019 - 2020</p>
           </div>
           <div className={Style.content}>
             <h4 className="font-bold">
               High School, <span className={Style.para}>ICSE Board</span>
             </h4>
-            <p className={Style.years}>2016 - 2017</p>
             <p>PCMB - 75.33%</p>
+            <p className={Style.years}>2016 - 2017</p>
           </div>
         </div>
       </div>
